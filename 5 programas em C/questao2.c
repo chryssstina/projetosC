5abdfd9d1dfddc04e35db7903e81b0cb85312e0c
chryssstina <144075668+chryssstina@users.noreply.github.com>
2023-10-28 14:42:01 -0300

    Update questao2.c

--- 5 programas em C/questao2.c
+++ 5 programas em C/questao2.c
@@ -14,16 +14,16 @@ int main(void) {
   scanf("%f", &numHora);
   printf("\n");
   
-  hora = (int)numHora; /*parte inteira*/
-  min = (numHora - (int)numHora) * 100; /*parte decimal*/
+  hora = (int)numHora; /*parte inteira do número digitado*/
+  min = (numHora - (int)numHora) * 100; /*parte decimal do número digitado*/
 
   if((hora>23 || hora<0) || (min>59 || min<0)){
-    printf("A parte inteira representa a hora. Assim ela só pode ir de 0 a 23.\n");
-    printf("A parte decimal representa os minutos. Assim ela só pode ir de 0 a 59.\n");
+      printf("A parte inteira representa a hora. Assim ela só pode ir de 0 a 23.\n");
+      printf("A parte decimal representa os minutos. Assim ela só pode ir de 0 a 59.\n");
   }else{
-    minTotal = hora*60 + min;
-    segTotal = minTotal*60;
-    printf("A hora que você digitou equivale a %d minutos ou %d segundos." ,minTotal,segTotal);
+      minTotal = hora*60 + min;
+      segTotal = minTotal*60;
+      printf("A hora que você digitou equivale a %d minutos ou %d segundos." ,minTotal,segTotal);
   }
 
   
