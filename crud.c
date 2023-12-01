6bdfb94d7a648c538b55e4496b1e3b4b052cc856
Chrystina <144075668+chryssstina@users.noreply.github.com>
2023-12-01 10:50:02 -0300

    Add files via upload

--- crud.c
+++ crud.c
@@ -2,9 +2,7 @@
 #define MAX 100
 
 
-int main(){
-    
-    struct dataAbast{
+struct dataAbast{
         int dia, mes, ano;
     };
     struct dataAbast sData;
@@ -20,50 +18,91 @@ int main(){
     struct dadosAbastecimento abastecimento;
     struct dadosAbastecimento abastecimento_vetor[MAX];
 
+//----------Funções----------
+void adicionarAbastecimento(void);
+void consultarAbastecimento();
+void editarAbastecimento();
+void excluirAbastecimento();
+void listarAbastecimento();
+int ultimo_codigo(void);
 
+//---------------------------
+
+int main(){
+    int opcao;
+
+    printf("Escolha uma opção; \n");
+    printf("1. Adicionar abastecimento");
+    printf("2. Consultar abastecimento");
+    printf("3. Editar abastecimento");
+    printf("4. Excluir abastecimento");
+    printf("5. Listar abastecimento");
+    printf("0. Sair");
+    
+    scanf("%d",&opcao);
+
+    while (opcao != 0){ 
+            if(opcao=1){
+                adicionarAbastecimento();
+            }
+            if(opcao=2){
+                consultarAbastecimento();
+            }
+            if(opcao=3){
+                editarAbastecimento();
+            }
+            if(opcao=4){
+                excluirAbastecimento();
+            }if(opcao=5){
+                listarAbastecimento();
+            }
+    }
+}
+
+int ultimo_codigo(void)
+{
     int cod=0;
 
-    for (int x=0; x < MAX; x++){
-            if (abastecimento[x].codigo > cod)
-              cod = abastecimento[x].codigo;
-    }
+    for (int x=0; x < MAX; x++)
+        if (abastecimento_vetor[x].codigo > cod)
+            cod = abastecimento_vetor[x].codigo;
 
-        
-        
+    return cod;
+}
+
+
+void adicionarAbastecimento(){
+
+    int x,cod;
 
     for(x=0; x< MAX; x++){
-        if(abastecimento[x].codigo == 0){
+        if(abastecimento_vetor[x].codigo == 0){
 
             cod = ultimo_codigo();
-            abastecimento[x].codigo = cod + 1;
+            abastecimento_vetor[x].codigo = cod + 1;
 
-            printf("Digite o código \n");
-            scanf("%d", &abastecimento[].codigo);
+            //printf("Digite o código \n");
+            //scanf("%d", &abastecimento_vetor[x].codigo);
 
             printf("Digite a data de abastecimento \n");
-            scanf("%d %d %d", &abastecimento.data.dia, &abastecimento.data.mes, &abastecimento.data.ano);
-
+            scanf("%d %d %d", &abastecimento_vetor[x].data.dia, &abastecimento.data.mes, &abastecimento.data.ano);
 
             printf("Digite o tipo de combustível (Gasolina/Alcool/Diesel)\n");
             fflush(stdin);
-            fgets(abastecimento.combustivel, 12, stdin);
+            fgets(abastecimento_vetor[x].combustivel, 12, stdin);
 
             printf("Digite a qualidade (Comum/Aditivada) \n");
             fflush(stdin);
-            fgets(abastecimento.qualidade, 12, stdin);
+            fgets(abastecimento_vetor[x].qualidade, 12, stdin);
 
             printf("Digite os litros \n");
-            scanf("%f", &abastecimento.litros);
+            scanf("%f", &abastecimento_vetor[x].litros);
 
             printf("Digite o preço\n");
-            scanf("%f", &abastecimento.preco);
+            scanf("%f", &abastecimento_vetor[x].preco);
+
+            break;
         }
-    }
-
-    
-
-
-    
-    
+    } 
 }
 
